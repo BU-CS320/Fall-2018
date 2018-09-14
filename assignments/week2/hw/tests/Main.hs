d5b214623d5f2efe4a4113e2e512317b0e0a6cfe
mikewintersjr <mikewintersjr@gmail.com>
2018-09-14 19:26:18 -0400

    added test case for lectureDay in April

--- assignments/week2/hw/tests/Main.hs
+++ assignments/week2/hw/tests/Main.hs
@@ -9,7 +9,7 @@ main = defaultMain unitTests
 unitTests =
   testGroup
     "Tests"
-    [andTrueTrue, mult3and5]
+    [andTrueTrue, mult3and5, lectureDayApril]
 
 -- this function converts from our custom Bools to the standard Bools so we have a lot of automatic things already defined
 fromhwBoolToStandardBool :: Hw.Bool -> Prelude.Bool
@@ -28,3 +28,7 @@ andTrueTrue =
 -- the defined numbers should add the correct way
 mult3and5 =
   testCase "3 * 5 = 15" $ assertEqual [] 15 (fromNatToInteger (Hw.mult Hw.three Hw.five))
+
+-- A lecture day in the months outside of the Fall 2018 Semester should not count
+lectureDayApril = 
+  testCase "lectureDay (Date April 3 Tuesday) = False" $ assertEqual [] Prelude.False (fromhwBoolToStandardBool (Hw.lectureDay (Hw.Date Hw.April Hw.three Hw.Tuesday)))
