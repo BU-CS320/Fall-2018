e44265432a1e753430e9c8167f6b0d957fa5f3ac
zxy315 <zxy315@bu.edu>
2018-10-02 17:06:59 -0400

    add IndexTree Test

--- assignments/week4/hw/tests/IndexTreeTest.hs
+++ assignments/week4/hw/tests/IndexTreeTest.hs
@@ -17,25 +17,25 @@ unitTests =
      test_equal]
 
 
--- create IndexTree: Node (Node (Node (Node (Node (Node Null 1 ["this"] Null) 2 ["homework"] Null) 3 ["is"] Null) 4 ["so"] Null) 5 ["damn"] Null) 6 ["hard"] Null
+-- create IndexTree: Node (Node (Node (Node (Node (Node Null 1 ["homework"] Null) 2 ["damn"] Null) 3 ["is"] Null) 4 ["this"] Null) 5 ["hard"] Null) 6 ["so"] Null
 
-exampleTree1 = insert 1 "this" (insert 2 "homework" (insert 3 "is" (insert 4 "so" (insert 5 "damn" (insert 6 "hard" Null)))))
+exampleTree1 = insert 4 "this" (insert 1 "homework" (insert 3 "is" (insert 6 "so" (insert 2 "damn" (insert 5 "hard" Null)))))
 
 
 test_lookupKey =
-  testCase "look for key 5 in exampleTree1" $ assertEqual [] (["damn"]) (lookupKey 5 exampleTree1)
+  testCase "look for key 5 in exampleTree1" $ assertEqual [] (["hard"]) (lookupKey 5 exampleTree1)
 
 test_member =
-  testCase "2 homework is in exampleTree1" $ assertBool [] (member 2 "homework" exampleTree1)
+  testCase "1 homework is in exampleTree1" $ assertBool [] (member 1 "homework" exampleTree1)
 
 test_keys =
   testCase "keys for exampleTree1" $ assertEqual [] ([1,2,3,4,5,6]) (keys exampleTree1)
 
 test_pairs =
-  testCase "pairs for exampleTree1" $ assertEqual [] ([(1,"this"),(2,"homework"),(3,"is"),(4,"so"),(5,"damn"),(6,"hard")]) (pairs exampleTree1)
+  testCase "pairs for exampleTree1" $ assertEqual [] ([(1,"homework"),(2,"damn"),(3,"is"),(4,"this"),(5,"hard"),(6,"so")]) (pairs exampleTree1)
 
 test_invert =
-  testCase "invert exampleTree1" $ assertEqual [] (insert "this" 1 (insert "homework" 2 (insert "is" 3 (insert "so" 4 (insert "damn" 5 (insert "hard" 6 Null)))))) (invert exampleTree1)
+  testCase "invert exampleTree1" $ assertEqual [] (insert "this" 4 (insert "homework" 1 (insert "is" 3 (insert "so" 6 (insert "damn" 2 (insert "hard" 5 Null)))))) (invert exampleTree1)
 
 test_equal =
   testCase "exampleTree1 == exampleTree1" $ assertBool [] (exampleTree1 == exampleTree1)
