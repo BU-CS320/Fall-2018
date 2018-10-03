acbe49d3a1294ac568f7cd645241c670bd7885a1
Mark Lemay <lemay@bu.edu>
2018-10-03 13:30:20 -0400

    spelling fix

--- assignments/week5/hw/tests/TypeclassProblemsTest.hs
+++ assignments/week5/hw/tests/TypeclassProblemsTest.hs
@@ -4,7 +4,7 @@ import Test.Tasty (testGroup)
 import Test.Tasty.HUnit (assertEqual, assertBool, testCase)
 import Test.Tasty.QuickCheck (testProperty )
 
-import TypeclassProblems(DayOfTheWeek, iSurewhishIHadThisFunction, forAll, boolEq)
+import TypeclassProblems(DayOfTheWeek, iSureWishIHadThisFunction, forAll, boolEq)
 
 
 unitTests =
