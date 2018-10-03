5f05afdab58cc5def91102e9ce34649d7a7aaeb4
Mark Lemay <lemay@bu.edu>
2018-10-03 18:52:43 -0400

    commented out bad test, fixed tabbing

--- assignments/week4/hw/tests/IndexAndConcordanceTest.hs
+++ assignments/week4/hw/tests/IndexAndConcordanceTest.hs
@@ -9,22 +9,23 @@ import IndexAndConcordance(cleanup,createWordLists,makePairs, addPageNumber, cre
 unitTests =
   testGroup
     "IndexAndConcordanceTest"
-    [cleanUpTest,createWordListsTest]
+    [cleanUpTest --,createWordListsTest
+	]
 
-    str1 = "Hey there student, I hope your test case did not FAIL. LOSER."
-    str1Ans = "hey there student  i hope your test case did not fail  loser "
+str1 = "Hey there student, I hope your test case did not FAIL. LOSER."
+str1Ans = "hey there student  i hope your test case did not fail  loser "
 
-    str2 = "Okay, this is string 2, it's not really very useful but it does the JOB."
+str2 = "Okay, this is string 2, it's not really very useful but it does the JOB."
 
-    strList = [str1, str2]
-    createWordListsAnswer = [["hey","there","student","i","hope","your","test","case","did","not","fail","loser"],["okay","this","is","string","2","it's","not","really","very","useful","but","it","does","the","job"]]
+strList = [str1, str2]
+createWordListsAnswer = [["hey","there","student","i","hope","your","test","case","did","not","fail","loser"],["okay","this","is","string","2","it's","not","really","very","useful","but","it","does","the","job"]]
 
-   
-    cleanUpTest = 
-    	testCase "should return: hey there student  i hope your test case did not fail  loser "  $ assertEqual [] (str1Ans) (cleanup str1)
 
-    createWordListsTest = 
-    	testCase "output is incorrect, please check test file for answer" $ assertEqual (createWordListsAnswer) (createWordLists strList)
+cleanUpTest = 
+	testCase "should return: hey there student  i hope your test case did not fail  loser "  $ assertEqual [] (str1Ans) (cleanup str1)
+
+--createWordListsTest = 
+--	testCase "output is incorrect, please check test file for answer" $ assertEqual (createWordListsAnswer) (createWordLists strList)
 
 
 
