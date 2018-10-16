29d4929fc3cd7437f2306ca7a3d1de27d2f469f1
mikewintersjr <mikewintersjr@gmail.com>
2018-10-16 14:35:59 -0400

    Update fibonacci test
    Fibonacci sequence should begin with 0.  https://oeis.org/A000045

--- assignments/week6/hw/tests/InfiniteListsTest.hs
+++ assignments/week6/hw/tests/InfiniteListsTest.hs
@@ -37,7 +37,7 @@ testG = testCase "testG: collatz 17 " $ assertEqual [] [17,52,26,13,40,20,10,5,1
 
 testH = testCase "testH: randomSequence 7927 3591 1 10000 " $ assertEqual [] [7927,5858,6079,9690,6791,6482,6863,5034,7095,8146] $  take 10 (randomSequence 7927 3591 1 10000)
 
-testI = testCase "testI: first 20 correct" $ assertEqual [] [1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765] $  take 20 fibonacci
+testI = testCase "testI: first 20 correct" $ assertEqual [] [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181] $  take 20 fibonacci
 
 testJ = testCase "testJ: first 10 correct" $ assertEqual [] [30,60,90,120,150,180,210,240,270,300] $  (take 10 hamming)
 
