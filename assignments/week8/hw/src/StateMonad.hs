f57f01854912161a53a651d64ec69e844a9a6e9b
Mark <lemay@bu.edu>
2018-10-24 10:16:48 -0400

    first lab convinced me to post the answers here

--- assignments/week8/hw/src/StateMonad.hs
+++ assignments/week8/hw/src/StateMonad.hs
@@ -1,14 +1,18 @@
 module StateMonad where
 import Control.Monad       (liftM, ap)
 
+-- notice how we handled state in https://github.com/BU-CS320/Fall-2018/blob/master/assignments/week7/hw/src/week5/Lang4.hs
+-- we can make a monadic type to handle the details for us
 data Stateful s a  = Stateful (s ->(a,s))
 
+-- a helper function to pull out the function bit
 app :: Stateful s a -> (s ->(a,s))
 app (Stateful stateful) = stateful
 
 instance Functor (Stateful s) where
   -- fmap :: (a -> b) -> State a -> State b
-  fmap f (Stateful sa) =  undefined
+  fmap f (Stateful sa) =  Stateful $ \ state -> case sa state of
+                                                  (a, output) -> (f a, output)
 
 --ignore this for now
 instance Applicative (Stateful s) where
@@ -17,13 +21,14 @@ instance Applicative (Stateful s) where
 
 instance Monad (Stateful s) where
   --return :: a -> State a
-  return a = undefined
+  return a = Stateful $  \ state -> (a, state)
 
   --(>>=) :: State a -> (a -> State b) -> State b
-  (Stateful sa) >>= f  = undefined
+  (Stateful sa) >>= f = Stateful $  \ state -> case sa state of
+                                                 (a, output) -> app (f a) output
 
 
+-- a function that gets the state (in a stateful way)
 -- stolen from https://wiki.haskell.org/State_Monad
-
 get :: Stateful s s
 get = Stateful $ \ s ->  (s,s)
