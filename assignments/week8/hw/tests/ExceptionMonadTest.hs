2971d6e0044f83cf5c055ea1d6ad2f05fcf6fcf0
Mark <lemay@bu.edu>
2018-10-24 19:10:19 -0400

    removed a confusing problem

--- assignments/week8/hw/tests/ExceptionMonadTest.hs
+++ assignments/week8/hw/tests/ExceptionMonadTest.hs
@@ -4,7 +4,7 @@ import Test.Tasty (testGroup)
 import Test.Tasty.HUnit (assertEqual, assertBool, testCase)
 import Test.Tasty.QuickCheck
 
-import ExceptionMonad (Unsafe(Error, Ok),listProb)
+import ExceptionMonad (Unsafe(Error, Ok))
 
 unitTests =
   testGroup
@@ -16,11 +16,10 @@ unitTests =
 instructorTests = testGroup
       "instructorTests"
       [
-      testCase "listProb works on 1 " $ assertEqual [] (Ok 0) $ listProb [Ok 0, Ok 0],
-      testCase "listProb works on 1 " $ assertEqual [] (Ok 5) $ listProb [Ok 0, Ok 2, Ok 3, Ok 0]
+      testCase "return test " $ assertEqual [] (Ok 0) $ return 0,
+      testCase "return test " $ assertEqual [] (Ok 5) $ return 5
       ]
 
 
 -- TODO: your tests here!!!
 -- TODO: Lots of tests for all the laws!
--- TODO: many more examples of the list problem
